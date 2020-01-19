4eab2a906ae7b40439a4483b889b3a3042a92af4
coltonfalkner <coltonfalkner@gmail.com>
2020-01-19 15:47:00 -0600

    Recalibrated answers to match characters

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,37 +2,37 @@ var friendsData = [
     {
       name: "Philip J. Fry",
       photo: "https://www.syfy.com/sites/syfy/files/styles/syfy_cast_detail_normal_wide_1x/public/cast_futurama_phillipjfry.jpg",
-      scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
+      scores: [4, 2, 1, 5, 2, 4, 3, 4, 5, 1]
     },
     {
       name: "Bender Bending Rodriguez",
       photo:"https://vignette.wikia.nocookie.net/villains/images/8/8f/The_Bender.jpg/revision/latest?cb=20180701200006",
-      scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+      scores: [5, 1, 1, 5, 2, 5, 3, 5, 1, 5]
     },
     {
       name: "Turanga Leela",
       photo:"https://www.syfy.com/sites/syfy/files/styles/1140x640/public/cast_futurama_turangaleela_0.jpg",
-      scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+      scores: [1, 4, 2, 3, 2, 4, 2, 1, 1, 1]
     },
     {
       name: "Professor Hubert Farnsworth",
       photo:"https://www.syfy.com/sites/syfy/files/styles/syfy_cast_detail_normal_wide_1x/public/cast_futurama_proffarnsworth.jpg",
-      scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+      scores: [5, 5, 3, 5, 5, 2, 2, 2, 5, 4]
     },
     {
       name: "Amy Wong",
       photo:"https://www.syfy.com/sites/syfy/files/styles/syfy_cast_detail_normal_wide_1x/public/cast_futurama_amywong_0.jpg",
-      scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5]
+      scores: [4, 4, 2, 5, 5, 5, 3, 2, 3, 1]
     },
     {
       name: "Hermes Conrad",
       photo:"https://www.syfy.com/sites/syfy/files/styles/syfy_cast_detail_normal_wide_1x/public/cast_futurama_conrad_0.jpg",
-      scores: [2, 4, 2, 4, 2, 3, 4, 4, 4, 3]
+      scores: [5, 1, 5, 3, 2, 2, 5, 3, 1, 5]
     },
     {
       name: "Dr. John A. Zoidberg",
       photo:"https://www.syfy.com/sites/syfy/files/styles/syfy_cast_detail_normal_wide_1x/public/cast_futurama_zoidberg.jpg",
-      scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
+      scores: [5, 5, 1, 5, 1, 1, 1, 5, 1, 1]
     }
   ];
   
