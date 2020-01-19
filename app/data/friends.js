9663f4d00f47662ac3e6d3f99bbc062bee00c520
coltonfalkner <coltonfalkner@gmail.com>
2020-01-19 14:39:32 -0600

    Created roster of friends for API list

---
+++ app/data/friends.js
@@ -0,0 +1,42 @@
+var friendsData = [
+    {
+      name: "Philip J. Fry",
+      photo: "https://www.syfy.com/sites/syfy/files/styles/syfy_cast_detail_normal_wide_1x/public/cast_futurama_phillipjfry.jpg",
+      scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
+    },
+    {
+      name: "Bender Bending Rodriguez",
+      photo:"https://vignette.wikia.nocookie.net/villains/images/8/8f/The_Bender.jpg/revision/latest?cb=20180701200006",
+      scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    },
+    {
+      name: "Turanga Leela",
+      photo:"https://www.syfy.com/sites/syfy/files/styles/1140x640/public/cast_futurama_turangaleela_0.jpg",
+      scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+      name: "Professor Hubert Farnsworth",
+      photo:"https://www.syfy.com/sites/syfy/files/styles/syfy_cast_detail_normal_wide_1x/public/cast_futurama_proffarnsworth.jpg",
+      scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+      name: "Amy Wong",
+      photo:"https://www.syfy.com/sites/syfy/files/styles/syfy_cast_detail_normal_wide_1x/public/cast_futurama_amywong_0.jpg",
+      scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5]
+    },
+    {
+      name: "Hermes Conrad",
+      photo:"https://www.syfy.com/sites/syfy/files/styles/syfy_cast_detail_normal_wide_1x/public/cast_futurama_conrad_0.jpg",
+      scores: [2, 4, 2, 4, 2, 3, 4, 4, 4, 3]
+    },
+    {
+      name: "Dr. John A. Zoidberg",
+      photo:"https://www.syfy.com/sites/syfy/files/styles/syfy_cast_detail_normal_wide_1x/public/cast_futurama_zoidberg.jpg",
+      scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
+    }
+  ];
+  
+  
+  
+  // Export to API Route
+  module.exports = friendsData;
